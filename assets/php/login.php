765271c1d283737ea90afed23cd945972ce67e7c
giuliano-villca <guilianox323@gmail.com>
2024-10-23 23:36:48 -0300

    nuevo login

--- assets/php/login.php
+++ assets/php/login.php
@@ -1,26 +1,26 @@
-
 <?php
 define("HOST", "localhost");
 define("DB", "hiper-card");
 define("USER","root");
 define("PASSWORD", "");
-$con = mysqli_connect(HOST, USER, PASSWORD, DB);
+$conn = mysqli_connect(HOST, USER, PASSWORD, DB);
 
 if (!$conn)
 {
     die ("No hay conexión: ".mysqli_connect_error());
 }
 
-$email= $_POST('email');
-$pass= $_POST('password');
+$email = $_POST["email"];
+$pass = md5($_POST["password"]);
 
-$query= mysqli_query($conn, "SELECT * FROM Clientes WHERE email= '".$email."' and contraseña ='".$pass."'");
+$query= mysqli_query($conn, "SELECT email, contraseña FROM Clientes WHERE email = '" . $email . "' and contraseña = '" . $pass . "'");
 $nr = mysqli_num_rows($query);
+
 if($nr == 1){
-    header("Location : supernidea.html");
+    header('Location: ../supernidea.html ');
 }
 else if ($nr == 0)
 {
-    header("Location : login.html");
+    header('Location: ../login.html');
 }
-?>
+?> 
