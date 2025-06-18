a2fbe88f1c544e6e82b3115822e6aef23fd2cf94
angela-baltierrez <angelabaltierrez@gmail.com>
2025-06-18 19:15:44 -0300

    avence pago

---
+++ pago.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../hiper_card/assets/css/pago.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="conter">
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light">....</a>
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light">....</a>
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light">...</a>
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light">...</a>
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light">...</a>
+            <a href="#" class="list-group-item list-group-item-action list-group-item-light">...</a>
+ 
+        <div>
+            <form method="POST" action="../hiper_card/assets/php/...">
+                  
+                
+                    <div class="form-group">
+                
+                    <div style="display: flex;">
+                    <input  type="text"placeholder="ingresa tu nombre"  style="display: inline-block; vertical-align: top;">
+                    <input type="text" placeholder="ingresa tu apellido" style="display: inline-block; vertical-align: top;">
+                    </div>
+                    <input type="password" id="password" name="password" placeholder="Ingresa tu numero de targeta" required autocomplete="current-password">
+
+                    <div style="display: flex;">
+                    <input  type="text"placeholder="MM/AA"  style="display: inline-block; vertical-align: top;">
+                    <input type="text" placeholder="ingrese " style="display: inline-block; vertical-align: top;">
+                    </div>
+                    </div>
+                
+                    <div class="form-group button-class">
+                    <button id= "submit" type="submit" style="color: #fff;">confirmar</button>
+                    </div>
+            </form>
+        </div>
+        </div>
+    </div>
+</body>
+<style>
+
+</style>
+</html>
